565af08da2eb907e98585495d842067f310f4935
oma16 <48384861+oma16@users.noreply.github.com>
2022-06-18 14:00:13 +0100

    game

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-from random import random
-from random import choice
+from random import Random
 
+while True:
+    R = "rock"
+    P =  "paper" 
+    S = "scissors"
 
-R = "rock"
-P =  "paper" 
-S = "scissors"
-
-gameoptions = [R, S, P]
-print("Enter your option:")
-# cpu = random.choice(gameoptions)
-cpu = "you"
-player = input()
-for i in gameoptions:
-    if i == R:
-        print(i ,":", cpu)
-    
-    elif i == S:
+    gameoptions = [R, S, P]
+    computeroption = Random.choice(gameoptions)
+    player = input("Enter your option(R or S or P):")
+    if player == (R or S or P):
+        print(f"\nplayer {player} : cpu {computeroption}")
+       
+    else:  
+         print("not amongst our options")  
         
-        print(i)
-    elif i == P:
-        print(i)
-    else:
-        print("not amongst our options")
+    if player == computeroption:
+            print("both player selected {player}. it is a tie")
+        
+    elif player == R:
+        if  computeroption == S:
+            print("Rock smahes Scissors! You win!")
+        else:
+            print("Paper covers Rock! You lose")    
 
+    elif player == P:
+        if  computeroption == R:
+            print("Paper covers Rock! You win") 
+            
+        else:
+            print("Scissors cuts paper! You lose!")
+    elif player == S:
+        if  computeroption == P:
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes Scissors! You lose")    
+
+    play_again = input("play again? (y/n): ")
+    if play_again.lower() != "y":
+       break
          
 
 
